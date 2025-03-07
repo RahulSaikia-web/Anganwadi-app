ff9cea61d981ed2bad48993ff94cfb637eb9728e
RahulSaikia-web <rahulsaikia682@gmail.com>
2025-03-07 23:50:50 +0530

    change option box color

--- app/WorkerHome.jsx
+++ app/WorkerHome.jsx
@@ -196,7 +196,7 @@ const styles = StyleSheet.create({
   optionText: {
     fontSize: 14,
     fontWeight: 'bold',
-    color: '#333',
+    color: '#8B0000',
     marginTop: 8,
     textAlign: 'center',
   },
