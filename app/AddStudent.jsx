dff1a7dc35eec3c7eb707caffbe598e11df44a68
uday <udaysubba2004@gmail.com>
2025-03-08 02:48:41 +0530

    fix add student

--- app/AddStudent.jsx
+++ app/AddStudent.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Alert, StyleSheet, Image, PermissionsAndroid, Platform } from 'react-native';
+import { View, Text, TextInput, Button, Alert, StyleSheet, Image} from 'react-native';
 import { Picker } from '@react-native-picker/picker';
-import * as ImagePicker from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
+import axios from 'axios';
 
+const FormData = global.FormData;
+  
 const AddStudent = () => {
   const [image, setImage] = useState();
-  const navigation = useNavigation();
   const apiUrl = 'https://magicminute.online/api';
   const [form, setForm] = useState({
     studentName: '',
@@ -22,48 +24,43 @@ const AddStudent = () => {
     setForm({ ...form, [name]: value });
   };
 
-  const requestCameraPermission = async () => {
-    if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.CAMERA,
-          {
-            title: 'Camera Permission',
-            message: 'This app needs access to your camera',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          }
-        );
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } catch (err) {
-        console.warn(err);
-        return false;
+  const loadImage = async (mode) => {
+    try{
+      let result = {};
+      if (mode == 'gallery'){
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+        result = await ImagePicker.launchImageLibraryAsync({
+          mediaType: ['image'],
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+          base64: true
+        });
+      }else{
+        await ImagePicker.requestCameraPermissionsAsync();
+        result = await ImagePicker.launchCameraAsync({
+          allowsEditing: false,
+          aspect: [1, 1],
+          base64: true,
+          quality: 1,
+        });
       }
-    }
-    return true;
-  };
 
-  const handleImagePick = () => {
-    ImagePicker.launchImageLibrary({ mediaType: 'photo', selectionLimit: 1 }, (response) => {
-      if (!response.didCancel && !response.error && response.assets.length > 0) {
-        setForm({ ...form, photo: response.assets[0] });
+      if (!result.canceled) {
+        await saveImage(result.assets[0]);
       }
-    });
-  };
-
-  const handleCameraCapture = async () => {
-    const hasPermission = await requestCameraPermission();
-    if (!hasPermission) {
-      Alert.alert('Permission Denied', 'Camera permission is required to take a photo');
-      return;
+    } catch (error){
+      alert("Error loading image: " + error);
     }
-    ImagePicker.launchCamera({ mediaType: 'photo', cameraType: 'back' }, (response) => {
-      if (!response.didCancel && !response.error && response.assets.length > 0) {
-        setForm({ ...form, photo: response.assets[0] });
-      }
-    });
-  };
+  }
+
+  const saveImage = async (image) => {
+    try {
+      setImage(image);
+    } catch (error){
+      throw error;
+    }
+  }
 
   const handleSubmit = async () => {
     if (Object.values(form).some((value) => !value)) {
@@ -79,29 +76,33 @@ const AddStudent = () => {
     formData.append('student_father_name', form.fatherName);
     formData.append('student_phone', form.phoneNumber);
     formData.append('student_center_id', form.centerId);
-    
-    if (form.photo) {
-      formData.append('student_image', {
-        uri: form.photo.uri,
-        type: form.photo.type,
-        name: form.photo.fileName,
-      });
-    }
-
+   
     try {
-      const myHeaders = new Headers();
-      myHeaders.append("Authorization", "Bearer "+access_token )
-      const response = await fetch(apiUrl+'/v1/students', {
-        method: 'POST',
-        headers: myHeaders,
-        body: formData,
-      });
-      
-      if (!response.ok) {
-        throw new Error('Failed to add student');
+      if (image){
+        formData.append('student_image_file', image.base64);
       }
-      
-      Alert.alert('Success', 'Student added successfully!');
+        
+        let config = {
+          method: 'post',
+          maxBodyLength: Infinity,
+          url: apiUrl+'/v1/students',
+          headers: {
+            'Authorization' : 'Bearer' + 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwianRpIjoiYzBmYWUxNjctM2VkMy00NDk5LWFmN2QtMDQ1ZjdiYTcyZTVjIiwidHlwZSI6ImFjY2VzcyIsImZyZXNoIjpmYWxzZSwiY3NyZiI6IiIsImlhdCI6MTc0MTM3ODgzOSwiZXhwIjoxNzQxNDE0ODM5LjIxNTgsInVzZXJfdHlwZSI6InN0YWZmIiwidXNlcl9pZCI6MiwidXNlcl9yb2xlIjoiV29ya2VyIn0.ScEZsQ1AYqpYikmClY4CkENr1CLLAlH6_kN0FP6CLEXmsh1RHoQUsN7o3_OcUFDYQRLg0Rg64HsXacgp2oe_4j88_gWT4ciFp-lpaq3kNQKgqkH9xQTIbErQX1eD4pU_m3fJh4SCZF6-zD3nVmeWgMZRUWgKMwqkhvHeYtVn9mzPtBexca651xDjm44pUjilyrYfgbAbd1iveD5qgLx9IlWt2rALjNLLYhMOa6-1A2rjuGhAIhO0K6jS4I1TipxoujhYaNg_BRQUvjo7P9ZEGqPZM-KWmMitVStxO5-4kgawnw31zxwtWMKDiNHqB3ZfkgBBkhDIijAmr7NRWpfFmw',
+            'Content-Type': 'multipart/form-data'
+          },
+          data: formData
+        };
+  
+        axios.request(config)
+        .then((response) => {
+          console.log(JSON.stringify(response.data));
+          Alert.alert('Success', 'Student added successfully!');
+        })
+        .catch((error) => {
+          Alert.alert('Error', 'Failed to add student nowwww!');
+          console.log(error.response.data);
+        });
+
     } catch (error) {
       console.log(error)
       Alert.alert('Error', 'Failed to add student here');
@@ -119,8 +120,9 @@ const AddStudent = () => {
       <Text style={styles.label}>Gender <Text style={styles.required}>*</Text></Text>
       <Picker style={styles.picker} selectedValue={form.gender} onValueChange={(value) => handleInputChange('gender', value)}>
         <Picker.Item label="Select Gender" value="" />
-        <Picker.Item label="Male" value="male" />
-        <Picker.Item label="Female" value="female" />
+        <Picker.Item label="Male" value="Male" />
+        <Picker.Item label="Female" value="Female" />
+        <Picker.Item label="Other" value="Other" />
       </Picker>
       
       <Text style={styles.label}>Mother Name <Text style={styles.required}>*</Text></Text>
@@ -137,11 +139,11 @@ const AddStudent = () => {
       
       <Text style={styles.label}>Photo <Text style={styles.required}>*</Text></Text>
       <View style={styles.buttonContainer}>
-        <Button title="Pick Photo" onPress={handleImagePick} />
+        <Button title="Pick Photo" onPress={() => { loadImage('gallery')}} />
         <View style={styles.buttonSpacing} />
-        <Button title="Take Photo" onPress={handleCameraCapture} />
+        <Button title="Take Photo" onPress={() => { loadImage()}} />
       </View>
-      {form.photo && <Image source={{ uri: form.photo.uri }} style={styles.image} />}
+      {image && <Image source={{ uri: image.uri }} style={styles.image} />}
       
       <View style={styles.buttonSpacing} />
       <Button title="Submit" onPress={handleSubmit} color="#28a745" />
@@ -150,39 +152,18 @@ const AddStudent = () => {
 };
 
 const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-    backgroundColor: '#f8f9fa',
-  },
-  navBar: {
-    width: '100%',
-    height: 60,
-    backgroundColor: 'darkred',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-    paddingHorizontal: 15,
-    paddingTop: 20,
-  },
-  backButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  backText: {
-    color: 'white',
-    fontSize: 18,
-    marginLeft: 5,
-  },
-  scrollContainer: {
-    flexGrow: 1,
-    padding: 20,
-  },
   container: {
+    padding: 20,
+    backgroundColor: '#f8f9fa',
     flex: 1,
   },
   label: {
     fontSize: 16,
     fontWeight: 'bold',
     marginBottom: 5,
+  },
+  required: {
+    color: 'red',
   },
   input: {
     borderWidth: 1,
