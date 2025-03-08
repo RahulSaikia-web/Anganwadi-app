c0c51265302ca1209f2328a4cc4d988517fc09aa
RahulSaikia-web <rahulsaikia682@gmail.com>
2025-03-08 23:25:29 +0530

    Fetch Worker details

--- app/WorkerHome.jsx
+++ app/WorkerHome.jsx
@@ -1,13 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { 
   View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, 
   Modal, Button ,Alert
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { FontAwesome5, Ionicons } from '@expo/vector-icons';
+import * as SecureStore from 'expo-secure-store';
 
+async function storeGetValueFor(key) {
+  let result = await SecureStore.getItemAsync(key);
+  if (result) {
+    return result;
+  }
+}
 const WorkerHome = () => {
   const navigation = useNavigation();
+  const [staffData,setstaffData] = useState()
   const [modalVisible, setModalVisible] = useState(false);
   const handleLogout = () => {
     Alert.alert('Logout', 'Are you sure you want to logout?', [
@@ -15,21 +23,49 @@ const WorkerHome = () => {
       { text: 'Logout', onPress: () =>  navigation.replace('index')},
     ]);
   };
-  const user = {
-    name: 'Rahul Saikia',
-    phone: '9387394822',
-    aadhar: '1234-5678-9012',
-    role: 'Worker',
-  };
+  // const user = {
+  //   name: 'Rahul Saikia',
+  //   phone: '9387394822',
+  //   aadhar: '1234-5678-9012',
+  //   role: 'Worker',
+  // };
+  useEffect(()=>{
+      getStudents()
+    },[])
+  
+    const getStudents = async ()=>{
+      let JWT_Token = await storeGetValueFor('JWT-Token');
 
+         const apiUrl = 'https://magicminute.online/api/v1/staffs/self/';
+        try {
+          const response = await fetch(apiUrl, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${JWT_Token}`, // Add the token here
+            },
+          });
+    
+          if (response.ok) {
+            const data = await response.json();            
+            setstaffData(data);
+            console.log(staffData);
+            
+          } else {
+            console.log('Failed to fetch  ');
+          }
+        } catch (error) {
+          console.error('Error fetching students:', error);
+        }
+      }
   return (
     <ScrollView style={styles.container}>
       {/* Profile Section */}
       <TouchableOpacity style={styles.profileSection} onPress={() => setModalVisible(true)}>
         <Image source={require('@/assets/images/profile.webp')} style={styles.profileImage} />
         <View>
-          <Text style={styles.userName}>{user.name}</Text>
-          <Text style={styles.userLocation}>Golaghat (18292051008)</Text>
+          <Text style={styles.userName}>{staffData ? staffData.staff_full_name : "Name"}</Text>
+          <Text style={styles.userLocation}>{staffData ? staffData.staff_phone : '0000000000'}</Text>
         </View>
       </TouchableOpacity>
 
@@ -65,7 +101,10 @@ const WorkerHome = () => {
         <OptionButton icon="cogs" text="Settings" onPress={() => navigation.navigate('Settings')} />
         <OptionButton icon="user-plus" text="Add Beneficiaries" onPress={() => navigation.navigate('AddBeneficiaries')} />
       </View>
-
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+              <Ionicons name="log-out-outline" size={20} color="white" />
+              <Text style={styles.logoutText}>Logout</Text>
+            </TouchableOpacity>
       {/* Profile Modal */}
       <Modal visible={modalVisible} animationType="slide" transparent={true}>
         <View style={styles.modalContainer}>
@@ -73,24 +112,21 @@ const WorkerHome = () => {
             <Text style={styles.modalTitle}>User Details</Text>
             <View style={styles.detailRow}>
               <Text style={styles.detailLabel}>Name:</Text>
-              <Text style={styles.detailValue}>{user.name}</Text>
+              <Text style={styles.detailValue}>{staffData ? staffData.staff_full_name : "Name"}</Text>
             </View>
             <View style={styles.detailRow}>
               <Text style={styles.detailLabel}>Phone:</Text>
-              <Text style={styles.detailValue}>{user.phone}</Text>
+              <Text style={styles.detailValue}>{staffData ? staffData.staff_phone : '0000000000'}</Text>
             </View>
             <View style={styles.detailRow}>
               <Text style={styles.detailLabel}>Aadhar:</Text>
-              <Text style={styles.detailValue}>{user.aadhar}</Text>
+              <Text style={styles.detailValue}>{staffData ? staffData.staff_aadhar : '0000000000'}</Text>
             </View>
             <View style={styles.detailRow}>
               <Text style={styles.detailLabel}>Role:</Text>
-              <Text style={styles.detailValue}>{user.role}</Text>
+              <Text style={styles.detailValue}>{staffData ? staffData.staff_role : 'Who are You'}</Text>
             </View>
-            <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
-              <Ionicons name="log-out-outline" size={20} color="white" />
-              <Text style={styles.logoutText}>Logout</Text>
-            </TouchableOpacity>
+           
             <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.closeButton}>
               <Text style={styles.closeText}>Close</Text>
             </TouchableOpacity>
